eea5e52f64e48dcf13f08458abc974c41da540a5
jbolanosc <josue.carit@outlook.com>
2020-12-01 10:31:42 -0600

    Visual changes finished

--- src/Components/Sellers/index.js
+++ src/Components/Sellers/index.js
@@ -3,13 +3,15 @@ import React from "react";
 export default function index() {
   return (
     <div className="container-fluid d-flex flex-column align-items-center justify-content-center">
-      <h1 className="covered mb-2 text-centered">Beneficios para vendedores</h1>
+      <h1 className="covered mb-2 text-center">Beneficios para vendedores</h1>
       <div className="row">
         <div className="col-md-6">
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">Abierto 24/7</h5>
-              <p className="card-text">
+              <h5 className="card-title text-primary text-center">
+                Abierto 24/7
+              </h5>
+              <p className="card-text text-center">
                 No te preocupes por las restricciones o por un horario, tu
                 tienda estará disponible en todo momento para que tus ventas
                 nunca bajen.
@@ -17,32 +19,36 @@ export default function index() {
             </div>
           </div>
 
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">Mayor alcance</h5>
-              <p className="card-text">
+              <h5 className="card-title text-primary text-center">
+                Mayor alcance
+              </h5>
+              <p className="card-text text-center">
                 Podrás llegar a clientes de partes donde nunca imaginaste,
                 locales e internacionales.
               </p>
             </div>
           </div>
 
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">
+              <h5 className="card-title text-primary text-center">
                 Herramientas para tu crecimiento
               </h5>
-              <p className="card-text">
+              <p className="card-text text-center">
                 Te ofrecemos las mejores herramientas para que manejes tus
                 inventarios, ventas y equipo de la mejor manera.
               </p>
             </div>
           </div>
 
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">Tiendas Fantasma</h5>
-              <p className="card-text">
+              <h5 className="card-title text-primary text-center">
+                Tiendas Fantasma
+              </h5>
+              <p className="card-text text-center">
                 Ofrecemos las herramientas necesarias para que gestiones tu
                 dienda solo online, reduciendo tus costos de una tienda formal,
                 solo para que almacenes tu inventario y vendas.
@@ -51,34 +57,36 @@ export default function index() {
           </div>
         </div>
         <div className="col-md-6">
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">
+              <h5 className="card-title text-primary text-center">
                 Personaliza tu tienda
               </h5>
-              <p className="card-text">
+              <p className="card-text text-center">
                 Te ofrecemos un diseño personalizado y un dominio URL
                 personalizado para que tengas tu tienda online 100% propia, con
                 todas las herramientas implementadas para vender.
               </p>
             </div>
           </div>
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">Reduce costos</h5>
-              <p className="card-text">
+              <h5 className="card-title text-primary text-center">
+                Reduce costos
+              </h5>
+              <p className="card-text text-center">
                 Con unos cuantos clicks tendrás tu tienda 100% funcional,
                 reduciendo costos de terceros y si quieres te puedes dedicar
                 solo a vender online.
               </p>
             </div>
           </div>
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">
+              <h5 className="card-title text-primary text-center">
                 Preocupate solo por vender
               </h5>
-              <p className="card-text">
+              <p className="card-text text-center">
                 Te ofrecemos una plataforma completamente equipada para que solo
                 te preocupes por crear tu tienda, productos y vender de una
                 manera ultra sencilla, nos encargamos de todo lo demás.
@@ -86,12 +94,12 @@ export default function index() {
             </div>
           </div>
 
-          <div className="card mb-3" style={{ width: "18rem" }}>
+          <div className="card mb-3">
             <div className="card-body">
-              <h5 className="card-title text-primary ">
+              <h5 className="card-title text-primary text-center">
                 Aceptamos todo tipo de tienda o producto
               </h5>
-              <p className="card-text">
+              <p className="card-text text-center">
                 Ayudamos a cualquier emprendedor o negocio que desee vender su
                 producto.
               </p>
--- src/Components/Users/index.js
+++ src/Components/Users/index.js
@@ -3,28 +3,27 @@ import React from "react";
 export default function index() {
   return (
     <div className="container-fluid d-flex flex-column align-items-center justify-content-center">
-      <h1 className="covered mb-2 text-centered">Beneficios para usuarios</h1>
+      <h1 className="covered mb-2 text-center">Beneficios para usuarios</h1>
       <div className="row">
         <div className="col-md-6">
           <div className="card bg-primary text-light mb-3">
             <div className="card-body">
-              <h5 className="card-title">
+              <h5 className="card-title text-center">
                 Encontras especificamente lo que buscas
               </h5>
-              <p className="card-text">
+              <p className="card-text text-center">
                 Cansado de dar vueltas en un centro comercial sin encontrar lo
                 que buscas, acá lo tendrás a un par de clicks.
               </p>
             </div>
           </div>
 
-          <div
-            className="card bg-primary text-light mb-3"
-            style={{ width: "18rem" }}
-          >
+          <div className="card bg-primary text-light mb-3">
             <div className="card-body">
-              <h5 className="card-title">Desde la comodidad de donde estes</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">
+                Desde la comodidad de donde estes
+              </h5>
+              <p className="card-text text-center">
                 ¿Cansado de presas, aglomeraciones, estres o tenes que ir de un
                 lugar a otro para comprar lo que deseas?. Aca tenes el mundo
                 desde la comodidad de tu hogar.
@@ -32,13 +31,10 @@ export default function index() {
             </div>
           </div>
 
-          <div
-            className="card bg-primary text-light mb-3"
-            style={{ width: "18rem" }}
-          >
+          <div className="card bg-primary text-light mb-3">
             <div className="card-body">
-              <h5 className="card-title">Seguridad y rapidez</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">Seguridad y rapidez</h5>
+              <p className="card-text text-center">
                 Compra de la manera más segura, sin salir de casa, con tiendas
                 locales recibís el pedido el mismo día.
               </p>
@@ -48,8 +44,10 @@ export default function index() {
         <div className="col-md-6">
           <div className="card bg-primary text-light mb-3">
             <div className="card-body">
-              <h5 className="card-title">Compras programables o recurrentes</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">
+                Compras programables o recurrentes
+              </h5>
+              <p className="card-text text-center">
                 ¿Compras algo regularmente?. Acá podes crear una orden
                 recurrente que llegará a tu casa al momento que desees.
               </p>
@@ -57,8 +55,10 @@ export default function index() {
           </div>
           <div className="card bg-primary text-light  mb-3">
             <div className="card-body">
-              <h5 className="card-title">Compras local o internacionalmente</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">
+                Compras local o internacionalmente
+              </h5>
+              <p className="card-text text-center">
                 Podes encontrar tiendas alrededor de tu ciudad, asi como también
                 productos y tiendas internacionales para que no falte de donde
                 elegir.
@@ -67,8 +67,10 @@ export default function index() {
           </div>
           <div className="card bg-primary text-light  mb-3">
             <div className="card-body">
-              <h5 className="card-title">Encontras las mejores ofertas</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">
+                Encontras las mejores ofertas
+              </h5>
+              <p className="card-text text-center">
                 Semanal y mensualmente lanzamos una revista con todas las
                 mejores ofertas de acuerdo a tus gustos, para que siempre
                 encuentres lo mejor y ahorres.
@@ -78,8 +80,8 @@ export default function index() {
 
           <div className="card bg-primary text-light  mb-3">
             <div className="card-body">
-              <h5 className="card-title">Suministros</h5>
-              <p className="card-text">
+              <h5 className="card-title text-center">Suministros</h5>
+              <p className="card-text text-center">
                 Podés encontrar todos los suministros necesarios para la
                 operación de tu empresa, te los llevamos hasta la puerta de tu
                 casa.
